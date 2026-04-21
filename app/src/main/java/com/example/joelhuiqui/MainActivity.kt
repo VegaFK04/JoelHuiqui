b8051c66ebcd7ed95f0eface7ffbc2dc444e4cf5
VegaFK04 <emmanuel.veg.duart@gmail.com>
2026-04-21 13:45:53 -0600

    sepalabola

--- app/src/main/java/com/example/joelhuiqui/MainActivity.kt
+++ app/src/main/java/com/example/joelhuiqui/MainActivity.kt
@@ -4,18 +4,29 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.onGloballyPositioned
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.IntOffset
+import androidx.compose.ui.unit.dp
+import com.example.joelhuiqui.ui.theme.CRUZAZUL.SensorAcel
 import com.example.joelhuiqui.ui.theme.CRUZAZUL.Sensor_temperatura
 import com.example.joelhuiqui.ui.theme.JoelHuiquiTheme
 
@@ -26,14 +37,29 @@ class MainActivity : ComponentActivity() {
         setContent {
             JoelHuiquiTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                   Tempscreen()
+                   //Tempscreen()
+                    Bolitascreen()
                 }
             }
         }
     }
 }
 
-
+@Composable
+fun Bolitascreen(){
+    val context= LocalContext.current
+    val sensor= remember { SensorAcel(context) }
+    DisposableEffect(Unit) {
+        onDispose { sensor.unregister() }
+    }
+    Box(modifier = Modifier.fillMaxSize()){
+        Box (Modifier.size(50.dp).offset{IntOffset(sensor.posx.value.toInt(),
+            sensor.posy.value.toInt())}.background(
+                color = Color.Red,
+                shape = CircleShape
+            ))
+    }
+}
 @Composable
 fun Tempscreen(){
     val context= LocalContext.current
--- app/src/main/java/com/example/joelhuiqui/ui/theme/CRUZAZUL/Acelerometro.kt
+++ app/src/main/java/com/example/joelhuiqui/ui/theme/CRUZAZUL/Acelerometro.kt
@@ -5,17 +5,49 @@ import android.hardware.Sensor
 import android.hardware.SensorEvent
 import android.hardware.SensorEventListener
 import android.hardware.SensorManager
+import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
 
-class Acelerometro(contexto: Context): ViewModel(), SensorEventListener {
-val sensormanager = contexto.getSystemService(Context.SENSOR_SERVICE) as SensorManager
+class SensorAcel (context: Context): ViewModel(), SensorEventListener{
+    //Sensor Manager
 
-    override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
+    val sensorManager= context.getSystemService(Context.SENSOR_SERVICE) as SensorManager
 
+    //Sensor Distancia
+    val distanciaSensor= sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)
+
+    //State para leer el sensor
+
+    val accelX= mutableStateOf(0f)
+    val accelY= mutableStateOf(0f)
+    val accelZ= mutableStateOf(0f)
+
+    val posx=mutableStateOf(0f)
+    val posy=mutableStateOf(0f)
+
+         //Constructor
+    init {
+        //registrar el listener
+        distanciaSensor.also {
+            sensorManager.registerListener(this, it, SensorManager.SENSOR_DELAY_NORMAL)
+        }
     }
+
+
+    override fun onAccuracyChanged(p0: Sensor?, p1: Int) {}
 
     override fun onSensorChanged(event: SensorEvent?) {
+        event.let {
+            accelX.value= it!!.values[0]
+            accelY.value= it!!.values[1]
+            accelZ.value= it!!.values[2]
 
+            posx.value+=it!!.values[0]
+            posy.value+=it!!.values[0]
+
+        }
+    }
+    fun unregister(){
+        sensorManager.unregisterListener(this)
     }
 }
-
